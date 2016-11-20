afb97c3029a10024b110f176547a3fec6a9f52c3
Bart Wijnants <bartwijnants@outlook.com>
2016-11-20 15:00:43 +0100

    numberGenerator

--- src/numberGenerator.js
+++ src/numberGenerator.js
@@ -1,7 +1,54 @@
-var numberGenerator = {
-    generate: function generate() {
-        return 1;
-    }
-};
+function NumberGenerator() {
+    var ranges = [
+        { prefix: 'B', min: 1, max: 15 },
+        { prefix: 'I', min: 16, max: 30 },
+        { prefix: 'N', min: 31, max: 45 },
+        { prefix: 'G', min: 46, max: 60 },
+        { prefix: 'O', min: 61, max: 75 },];
 
-module.exports = numberGenerator;
+    this.generate = function generate(prefix) {
+        if (!prefix) {
+            throw Error('no prefix provided');
+        }
+        var range = getRange(prefix);
+        if (!range) {
+            throw Error('invalid prefix');
+        }
+        var number = randomIntFromInterval(range.min, range.max);
+        var zeroFilled = ('00' + number).slice(-2);
+        return range.prefix + zeroFilled;
+    };
+
+    function getRange(prefix) {
+        for (var i = 0; i < ranges.length; i++) {
+            if (ranges[i].prefix === prefix) {
+                return ranges[i];
+            }
+        }
+        return null;
+    }
+
+    function randomIntFromInterval(min, max)
+    {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+/*
+var prefixes = ['B', 'I', 'N', 'G', 'O'];
+
+var numberGenerator = {
+    generate: function generate(prefix) {
+        var randomPrefixIndex = randomIntFromInterval(0, prefixes.length - 1);
+        var prefix = prefixes[randomPrefixIndex];
+        var number = randomIntFromInterval(1, 75);
+        var zeroFilled = ('00' + number).slice(-2);
+        return prefix + zeroFilled;
+
+        function randomIntFromInterval(min, max)
+        {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+    }    
+};*/
+
+module.exports = NumberGenerator;
--- test/numberGenerator.test.js
+++ test/numberGenerator.test.js
@@ -1,12 +1,74 @@
-var numberGenerator = require('../src/numberGenerator.js');
+var NumberGenerator = require('../src/numberGenerator.js');
 var expect = chai.expect;
 
-describe('numberGenerator', function () {
-    describe('generate', function () {
-        it('given a numberGenerator when generate then 1 is returned', function () {
-            var result = numberGenerator.generate();
+describe('numberGenerator', function() {
+    describe('generate', function() {
+        var numberGenerator;
+        beforeEach(function() {
+            numberGenerator = new NumberGenerator();
+        });
 
-            expect(result).to.equal(1);
+        it('given a numberGenerator when generate for B then the result is 3 characters long starts with B and is withing 1 and 15', function() {
+            for (var i = 0; i < 100; i++) {
+                var result = numberGenerator.generate('B');
+
+                expect(result).to.be.a('string');
+                expect(result).to.have.lengthOf(3);
+                expect(result[0]).to.equal('B');
+                expect(result.substring(1)).to.be.within(1, 15);
+            }
+        });
+
+        it('given a numberGenerator when generate for I then the result is 3 characters long starts with I and is withing 16 and 30', function() {
+            for (var i = 0; i < 300; i++) {
+                var result = numberGenerator.generate('I');
+
+                expect(result).to.be.a('string');
+                expect(result).to.have.lengthOf(3);
+                expect(result[0]).to.equal('I');
+                expect(result.substring(1)).to.be.within(16, 30);
+            }
+        });
+
+        it('given a numberGenerator when generate for N then the result is 3 characters long starts with N and is withing 31 and 45', function() {
+            for (var i = 0; i < 300; i++) {
+                var result = numberGenerator.generate('N');
+
+                expect(result).to.be.a('string');
+                expect(result).to.have.lengthOf(3);
+                expect(result[0]).to.equal('N');
+                expect(result.substring(1)).to.be.within(31, 45);
+            }
+        });
+
+        it('given a numberGenerator when generate for G then the result is 3 characters long starts with G and is withing 46 and 60', function() {
+            for (var i = 0; i < 300; i++) {
+                var result = numberGenerator.generate('G');
+
+                expect(result).to.be.a('string');
+                expect(result).to.have.lengthOf(3);
+                expect(result[0]).to.equal('G');
+                expect(result.substring(1)).to.be.within(46, 60);
+            }
+        });
+
+        it('given a numberGenerator when generate for O then the result is 3 characters long starts with O and is withing 61 and 75', function() {
+            for (var i = 0; i < 300; i++) {
+                var result = numberGenerator.generate('O');
+
+                expect(result).to.be.a('string');
+                expect(result).to.have.lengthOf(3);
+                expect(result[0]).to.equal('O');
+                expect(result.substring(1)).to.be.within(61, 75);
+            }
+        });
+
+        it('given a numberGenerator when generate without parameter then an error is thrown', function() {
+                expect(numberGenerator.generate).to.throw(Error, 'no prefix provided');
+        });
+
+        it('given a numberGenerator when generate for a character not from BINGO then an error is thrown', function() {
+                expect(function() { numberGenerator.generate('e'); }).to.throw(Error, 'invalid prefix');
         });
     });
 });
