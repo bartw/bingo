b7f6b77713dbfd33b8a3a4432f301ba5d8fda7af
Bart Wijnants <bartwijnants@outlook.com>
2016-11-21 19:10:06 +0100

    basic ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,36 @@
 (function () {
     'use strict';
 
-    document.addEventListener('DOMContentLoaded', function () {});
+    document.addEventListener('DOMContentLoaded', function () {
+        var Game = require('./game.js');
+        var game = new Game();
+
+        var generatedNumbers = document.getElementById('generated-numbers');
+        var generatedNumber = document.getElementById('generated-number');
+        var nextNumber = document.getElementById('next-number');
+        var newGame = document.getElementById('new-game');
+        var allNumbersAreGenerated = 'all numbers are generated';
+
+        nextNumber.addEventListener('click', function() {
+            if (generatedNumber.textContent && generatedNumber.textContent !== allNumbersAreGenerated) {
+                var nextNumberListItem = document.createElement('li');
+                nextNumberListItem.textContent = generatedNumber.textContent;
+                generatedNumbers.appendChild(nextNumberListItem);
+            }
+
+            if (game.areNumbersAvailable()) {
+                var nextNumber = game.nextNumber();
+                generatedNumber.textContent = nextNumber;
+            } else {
+                generatedNumber.textContent = allNumbersAreGenerated;
+            }
+        });
+
+        newGame.addEventListener('click', function() {
+            game = new Game();
+            while (generatedNumbers.firstChild) {
+                generatedNumbers.removeChild(generatedNumbers.firstChild);
+            }
+        });
+    });
 })();
